d7ebafea487e0ede90c5dc4ba9fe60bb8584cde9
BlueCat Software <73719772+BlueCatSoftware@users.noreply.github.com>
2022-01-11 14:30:18 +0100

    Update PickerlyAdapter.java

--- Library/src/main/java/com/pickerly/imagespicker/Adapter/PickerlyAdapter.java
+++ Library/src/main/java/com/pickerly/imagespicker/Adapter/PickerlyAdapter.java
@@ -32,7 +32,6 @@ public class PickerlyAdapter extends RecyclerView.Adapter<PickerlyAdapter.ViewHo
     private boolean singleSelect;
     private boolean selectionMode;
 
-    // data is passed into the constructor
     public PickerlyAdapter(Context context, ArrayList<String> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
@@ -46,7 +45,6 @@ public class PickerlyAdapter extends RecyclerView.Adapter<PickerlyAdapter.ViewHo
         this.listener = listener;
     }
 
-    // inflates the row layout from xml when needed
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -54,54 +52,45 @@ public class PickerlyAdapter extends RecyclerView.Adapter<PickerlyAdapter.ViewHo
         return new ViewHolder(view);
     }
 
-    // binds the data to the TextView in each row
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // inflate views
-        View view = holder.itemView;
-        ImageView iconPic = view.findViewById(R.id.iconPic);
-        ImageView check = view.findViewById(R.id.check);
-        LinearLayout shadow = view.findViewById(R.id.shadow);
-        RelativeLayout back = view.findViewById(R.id.linear1);
 
-        // initialize data
-        check.setColorFilter(Color.parseColor("#FFFFFF"));
+        holder.check.setColorFilter(Color.parseColor("#FFFFFF"));
         String url = mData.get(position);
-        Glide.with(view)
+        Glide.with(holder.view)
                 .load(url)
                 .centerCrop()
                 .placeholder(R.drawable.ic_gallery)
                 .useAnimationPool(true)
-                .into(iconPic);
-        shadow.setVisibility(View.GONE);
-        check.setVisibility(View.GONE);
+                .into(holder.iconPic);
+        holder.shadow.setVisibility(View.GONE);
+        holder.check.setVisibility(View.GONE);
 
         if (multiSelectedPaths.contains(mData.get(position))) {
-            // check.setAlpha(1.0f);
-            shadow.setVisibility(View.VISIBLE);
-            check.setVisibility(View.VISIBLE);
+            holder.shadow.setVisibility(View.VISIBLE);
+            holder.check.setVisibility(View.VISIBLE);
         } else {
-            check.setVisibility(View.GONE);
-            shadow.setVisibility(View.GONE);
+            holder.check.setVisibility(View.GONE);
+            holder.shadow.setVisibility(View.GONE);
         }
 
         // selection listeners
-        back.setOnClickListener(arg0 -> {
+        holder.back.setOnClickListener(arg0 -> {
             if (singleSelect) {
                 listener.onPicSelected(mData.get(position));
             }
             if (!singleSelect) {
                 if (multiSelectedPaths.contains(mData.get(position))) {
                     multiSelectedPaths.remove(mData.get(position));
-                    shadow.setVisibility(View.GONE);
-                    check.setVisibility(View.GONE);
-                    shadow.setAlpha(1.0f);
+                    holder.shadow.setVisibility(View.GONE);
+                    holder.check.setVisibility(View.GONE);
+                    holder.shadow.setAlpha(1.0f);
                     String[] data = multiSelectedPaths.toArray( new String[multiSelectedPaths.size()]);
                     listener.onMultiplePicSelected(data);
                 } else {
-                    shadow.setAlpha(0.5f);
-                    shadow.setVisibility(View.VISIBLE);
-                    check.setVisibility(View.VISIBLE);
+                    holder.shadow.setAlpha(0.5f);
+                    holder.shadow.setVisibility(View.VISIBLE);
+                    holder.check.setVisibility(View.VISIBLE);
                     multiSelectedPaths.add(mData.get(position));
                     String[] data = multiSelectedPaths.toArray(new String[multiSelectedPaths.size()]);
                     listener.onMultiplePicSelected(data);
@@ -110,18 +99,15 @@ public class PickerlyAdapter extends RecyclerView.Adapter<PickerlyAdapter.ViewHo
         });
     }
 
-    // total number of rows
     @Override
     public int getItemCount() {
         return mData.size();
     }
 
-    // convenience method for getting data at click position
     String getItem(int id) {
         return mData.get(id);
     }
 
-    // allows clicks events to be caught
     void setClickListener(ItemClickListener itemClickListener) {
         this.mClickListener = itemClickListener;
     }
@@ -142,30 +128,31 @@ public class PickerlyAdapter extends RecyclerView.Adapter<PickerlyAdapter.ViewHo
         public void onMultiplePicSelected(String[] paths);
     }
 
-    // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
         void onItemClick(View view, int position);
     }
 
-    // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        ImageView imageView;
-        RelativeLayout bg;
+        
+		View view;
+        ImageView iconPic;
         ImageView check;
+        LinearLayout shadow;
+        RelativeLayout back;
 
         ViewHolder(View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.iconPic);
-            check = itemView.findViewById(R.id.check);
-            bg = itemView.findViewById(R.id.linear1);
+			view = itemView;
+            iconPic = view.findViewById(R.id.iconPic);
+			check = view.findViewById(R.id.check);
+			shadow = view.findViewById(R.id.shadow);
+			back = view.findViewById(R.id.linear1);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), "test picking 2", Toast.LENGTH_LONG).show();
             mClickListener.onItemClick(view, getAdapterPosition());
-            // listener.onPicSelected(mData.get(getAdapterPosition()));
         }
     }
 }
